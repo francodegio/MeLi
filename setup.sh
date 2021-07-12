16d71994104bbb68c2c6585ea99a6952bc2b3e12
francodegio <32992176+francodegio@users.noreply.github.com>
2021-07-11 21:15:22 -0300

    added line to install package in running environment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 python -m pip install -r requirements.txt
 python -m spacy download 'es_core_news_md'
+python -m pip install -e .
 gdown --id 1n2ISciX4_SACOrluj5ZWtjWTejIqyex8
 tar -xvf ml_evaluation.tar.gz
